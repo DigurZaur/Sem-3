04492eeccf4205503dc72b28e4a2ac8a9de663c7
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-28 11:59:01 +0300

    Complete Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте массив. Программа определяет, присутствует ли
+// заданное число в массиве. Ответ: Да/Нет
+
+int[] array = new int[5];
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string answer = "Нет";
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,17);    
+    System.Console.Write(array[i]+ " ");
+    if (num == array[i])
+    {
+        answer = "Да";
+    }
+}
+System.Console.WriteLine(answer);
