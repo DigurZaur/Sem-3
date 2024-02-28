7d8932c22be8183a703003e10c639750267b3737
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-28 12:16:09 +0300

    Complete Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте массив из 10 элементов, заполненный числами из
+// промежутка [-10;10]. Замените все отрицательные элементы
+// на положительные, а положительные на отрицательные.
+
+int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10,11);
+    System.Console.Write(array[i]+ "\t");    
+}
+
+System.Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = array[i] * -1;
+    System.Console.Write(array[i]+ "\t"); 
+}
